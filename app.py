1cbd07b67063a987d20e3efd5a8ed8b4d0c34acd
Akshat Pandey <147436589+weird-akshat@users.noreply.github.com>
2025-04-14 22:34:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def db_connection():
     try:
         # Get database URL from environment variable for Render
         database_url = os.environ.get('DATABASE_URL')
-        if not database_url:
+        # if not database_url:
             # Fallback for local development
-            database_url = "postgresql://postgres:rems@db.cmhffuysnjuhrltzlapq.supabase.co:5432/postgres"
+            # database_url = "postgresql://real_estate_1gxy_user:kmGTDSSqiUPpGUTPEb898MboFTq4wrzD@dpg-cvujv724d50c73b1q54g-a.oregon-postgres.render.com/real_estate_1gxy"
         
         conn = psycopg2.connect(database_url)
         conn.autocommit = False  # For transaction control
